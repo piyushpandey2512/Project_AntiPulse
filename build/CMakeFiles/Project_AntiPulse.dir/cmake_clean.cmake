2ad405a9c0bfb2435f637f7174de3a9c92d1e21a
piyushpandey2512 <piyushpandey2512@mail.com>
2025-06-12 21:25:34 +0200

    Stop tracking large files and update .gitignore

--- build/CMakeFiles/Project_AntiPulse.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Project_AntiPulse"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Project_AntiPulse.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
